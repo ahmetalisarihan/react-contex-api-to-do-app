0e4eea1a405142d515d1f301d8fc27b46fc07787
ahmetalisarihan <ahmetalisarihan@gmail.com>
2023-02-28 23:13:06 +0300

    Project Completed

--- src/components/ContentFooter.jsx
+++ src/components/ContentFooter.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useTodo } from "../contexts/TodoContext";
 
 function ContentFooter() {
+  const { todos, setTodos, filter, setFilter } = useTodo();
+  const clearCopleted = ()=> {
+	setTodos((prev)=>prev.filter((todo)=>!todo.completed))
+  }
+
   return (
     <footer className="footer">
-		<span className="todo-count">
-			<strong>2</strong>
-			items left
-		</span>
+      <span className="todo-count">
+        <strong>{todos.length} </strong>
+        item{todos.length > 1 && "s"} left
+      </span>
 
-		<ul className="filters">
-			<li>
-				<a href="#/" className="selected">All</a>
-			</li>
-			<li>
-				<a href="#/">Active</a>
-			</li>
-			<li>
-				<a href="#/">Completed</a>
-			</li>
-		</ul>
+      <ul className="filters">
+        <li>
+          <a
+            href="#/"
+            onClick={() => setFilter("all")}
+            className={filter === "all" ? "selected" : ""}
+          >
+            All
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            onClick={() => setFilter("active")}
+            className={filter === "active" ? "selected" : ""}
+          >
+            Active
+          </a>
+        </li>
+        <li>
+          <a
+            href="#/"
+            onClick={() => setFilter("completed")}
+            className={filter === "completed" ? "selected" : ""}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
 
-		<button className="clear-completed">
-			Clear completed
-		</button>
-	</footer>
-  )
+      <button className="clear-completed" onClick={clearCopleted}>Clear completed</button>
+    </footer>
+  );
 }
 
-export default ContentFooter
+export default ContentFooter;
